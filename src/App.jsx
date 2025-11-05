811018e1070b19321ef9f9027f952c340a542e40
Weligton <weligton0ferreira@gmail.com>
2025-11-05 07:06:28 +0700

    feat(App): add button

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import { Botao } from "./components/Botao"
 export default function App() {
   return (
     <>
-      <div>
-        <p>Hello World</p>
-        
-        <Botao />
-      </div>
+      <main>
+        <div>
+          <p>Hello World</p>
+          
+          <Botao corDeFundo="blue">Clique aqui</Botao>
+          <Botao corDeFundo="green">Clique aqui</Botao>
+          <Botao corDeFundo="brown">Clique aqui</Botao>
+        </div>
+      </main>
     </>
   )
 }
